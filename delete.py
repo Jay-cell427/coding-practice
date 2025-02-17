68b33be9740bcfdd653ac4aa65caf9aec8f05ce4
Jay-cell427 <jkifendi674@gmail.com>
2025-02-17 12:12:15 +0300

    Add files via upload

---
+++ delete.py
@@ -0,0 +1,12 @@
+def delete_nth(order, max_e):
+    num1=[]
+    count={}
+    for num i order:
+          if num not in count:
+             count[num] +=0
+    if count[num]< max_e:
+       num1.append(i)
+       count[num] +=1
+        return num1
+        
+   
